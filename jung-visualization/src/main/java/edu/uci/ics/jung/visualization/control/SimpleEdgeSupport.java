1de6ed38e7411cbdba2dd3e8e7d551fcbb410cc4
Tom Nelson <thomas.a.nelson@gmail.com>
2017-09-28 15:11:06 -0400

    fixed GraphEditorDemo  Changes to be committed: 	modified:   jung-samples/src/main/java/edu/uci/ics/jung/samples/GraphEditorDemo.java 	modified:   jung-visualization/src/main/java/edu/uci/ics/jung/visualization/control/EdgeSupport.java 	modified:   jung-visualization/src/main/java/edu/uci/ics/jung/visualization/control/EditingGraphMousePlugin.java 	modified:   jung-visualization/src/main/java/edu/uci/ics/jung/visualization/control/SimpleEdgeSupport.java

--- jung-visualization/src/main/java/edu/uci/ics/jung/visualization/control/SimpleEdgeSupport.java
+++ jung-visualization/src/main/java/edu/uci/ics/jung/visualization/control/SimpleEdgeSupport.java
@@ -57,6 +57,14 @@ public class SimpleEdgeSupport<V, E> implements EdgeSupport<V, E> {
     edgeEffects.endArrowEffects(vv);
   }
 
+  @Override
+  public void abort(BasicVisualizationServer<V, E> vv) {
+    startVertex = null;
+    edgeEffects.endEdgeEffects(vv);
+    edgeEffects.endArrowEffects(vv);
+    vv.repaint();
+  }
+
   public EdgeEffects<V, E> getEdgeEffects() {
     return edgeEffects;
   }
