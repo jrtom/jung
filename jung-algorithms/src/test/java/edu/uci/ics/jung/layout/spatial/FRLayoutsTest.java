e30a4f3584cdd374425ad36521b4a2477285647a
Tom Nelson <thomas.a.nelson@gmail.com>
2018-01-05 13:48:44 -0500

    javadoc and spelling corrections  Changes to be committed: 	modified:   jung-algorithms/src/test/java/edu/uci/ics/jung/layout/spatial/FRLayoutsTest.java 	modified:   jung-algorithms/src/test/java/edu/uci/ics/jung/layout/spatial/FRLayoutsTimingTest.java

--- jung-algorithms/src/test/java/edu/uci/ics/jung/layout/spatial/FRLayoutsTest.java
+++ jung-algorithms/src/test/java/edu/uci/ics/jung/layout/spatial/FRLayoutsTest.java
@@ -21,13 +21,20 @@ import org.slf4j.LoggerFactory;
 
 /**
  * This test makes a very small graph, sets initial locations for each node, and each time, it runs
- * a version of FRLayout. FRLayoutAlgorithm - the JUNG legacy version FRBHLayoutAlgorithm - modified
- * to use a BarnesHutQuadTree to reduce the number of repulsion comparisons with a custom Iterator
- * FRBHVisitorLayoutAlgorithm - modified to use the BarnesHutQuadTree as a visitor during the
- * repulsion step. The LayoutModel is subclassed so that no relax thread is started. A total of 200
- * steps of the layout relax is run. After all tests are run, the end values for both BarnesHut
- * versions are compared. The end values should be very close. The standard FRLayoutAlgorithm will
- * vary because force comparisions are approximated in the BarnesHut versions
+ * a version of FRLayout.
+ *
+ * <ul>
+ *   <li>FRLayoutAlgorithm - the JUNG legacy version
+ *   <li>FRBHLayoutAlgorithm - modified to use a BarnesHutQuadTree to reduce the number of repulsion
+ *       comparisons with a custom Iterator
+ *   <li>FRBHVisitorLayoutAlgorithm - modified to use the BarnesHutQuadTree as a visitor during the
+ *       repulsion step.
+ * </ul>
+ *
+ * <p>The LayoutModel is subclassed so that no relax thread is started. A total of 200 steps of the
+ * layout relax is run. After all tests are run, the end values for both BarnesHut versions are
+ * compared. The end values should be very close. The standard FRLayoutAlgorithm will vary because
+ * force comparisons are approximated in the BarnesHut versions
  *
  * <p>The Iterator version of BarnesHut uses storage space to cache collections of 'nodes' (or force
  * vectors) to compare with. The Visitor version does not use that additional storage space, so it
@@ -85,7 +92,7 @@ public class FRLayoutsTest {
   }
 
   @Test
-  public void testFRBHVisisor() {
+  public void testFRBHVisitor() {
     FRBHVisitorLayoutAlgorithm layoutAlgorithmThree = new FRBHVisitorLayoutAlgorithm();
     // using the same random seed each time for repeatable results from each test.
     layoutAlgorithmThree.setRandomSeed(0);
--- jung-algorithms/src/test/java/edu/uci/ics/jung/layout/spatial/FRLayoutsTimingTest.java
+++ jung-algorithms/src/test/java/edu/uci/ics/jung/layout/spatial/FRLayoutsTimingTest.java
@@ -15,18 +15,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This test makes a very small graph, sets initial locations for each node, and each time, it runs
- * a version of FRLayout. FRLayoutAlgorithm - the JUNG legacy version FRBHLayoutAlgorithm - modified
- * to use a BarnesHutQuadTree to reduce the number of repulsion comparisons with a custom Iterator
- * FRBHVisitorLayoutAlgorithm - modified to use the BarnesHutQuadTree as a visitor during the
- * repulsion step. The LayoutModel is subclassed so that no relax thread is started. A total of 200
- * steps of the layout relax is run. After all tests are run, the end values for both BarnesHut
- * versions are compared. The end values should be very close. The standard FRLayoutAlgorithm will
- * vary because force comparisions are approximated in the BarnesHut versions
+ * Measures the time required to do the same work with each of 3 versions of the FRLayoutAlgorithm:
  *
- * <p>The Iterator version of BarnesHut uses storage space to cache collections of 'nodes' (or force
- * vectors) to compare with. The Visitor version does not use that additional storage space, so it
- * should be better.
+ * <ul>
+ *   <li>FRLayoutAlgorithm - the JUNG legacy version
+ *   <li>FRBHLayoutAlgorithm - modified to use a BarnesHutQuadTree to reduce the number of repulsion
+ *       comparisons with a custom Iterator
+ *   <li>FRBHVisitorLayoutAlgorithm - modified to use the BarnesHutQuadTree as a visitor during the
+ *       repulsion step
+ * </ul>
  *
  * @author Tom Nelson
  */
@@ -65,7 +62,7 @@ public class FRLayoutsTimingTest {
   }
 
   @Test
-  public void testFRBHVisisor() {
+  public void testFRBHVisitor() {
     FRBHVisitorLayoutAlgorithm layoutAlgorithmThree = new FRBHVisitorLayoutAlgorithm();
     // using the same random seed each time for repeatable results from each test.
     layoutAlgorithmThree.setRandomSeed(0);
