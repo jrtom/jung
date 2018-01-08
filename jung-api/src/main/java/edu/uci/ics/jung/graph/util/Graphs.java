dee57f06aff0c67cba35043f82d71843759d1508
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2018-01-08 00:57:53 +0000

    Reduce memory usage(?) and de-anonymise impl. set class

--- jung-api/src/main/java/edu/uci/ics/jung/graph/util/Graphs.java
+++ jung-api/src/main/java/edu/uci/ics/jung/graph/util/Graphs.java
@@ -3,18 +3,20 @@ package edu.uci.ics.jung.graph.util;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toMap;
 
 import com.google.common.collect.AbstractIterator;
 import com.google.common.graph.AbstractNetwork;
 import com.google.common.graph.ElementOrder;
 import com.google.common.graph.EndpointPair;
 import com.google.common.graph.Graph;
-import com.google.common.graph.MutableGraph;
 import com.google.common.graph.MutableNetwork;
 import com.google.common.graph.Network;
 import java.util.AbstractSet;
 import java.util.ArrayDeque;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
@@ -38,25 +40,32 @@ public class Graphs {
   }
 
   public static <N> Set<N> topologicallySortedNodes(Graph<N> graph) {
-    checkNotNull(graph, "graph");
     // TODO: Do we want this method to be lazy or eager?
-    return new AbstractSet<N>() {
-      @Override
-      public Iterator<N> iterator() {
-        return new TopologicalOrderIterator<>(graph);
-      }
+    return new TopologicallySortedNodes<>(graph);
+  }
 
-      @Override
-      public int size() {
-        return graph.nodes().size();
-      }
-    };
+  private static class TopologicallySortedNodes<N> extends AbstractSet<N> {
+    private final Graph<N> graph;
+
+    private TopologicallySortedNodes(Graph<N> graph) {
+      this.graph = checkNotNull(graph, "graph");
+    }
+
+    @Override
+    public Iterator<N> iterator() {
+      return new TopologicalOrderIterator<>(graph);
+    }
+
+    @Override
+    public int size() {
+      return graph.nodes().size();
+    }
   }
 
   private static class TopologicalOrderIterator<N> extends AbstractIterator<N> {
     private final Graph<N> graph;
     private final Queue<N> roots;
-    private final MutableGraph<N> edgesRemaining;
+    private final Map<N, Integer> nonRootToInDegree;
 
     private TopologicalOrderIterator(Graph<N> graph) {
       this.graph = checkNotNull(graph, "graph");
@@ -66,8 +75,12 @@ public class Graphs {
               .stream()
               .filter(node -> graph.inDegree(node) == 0)
               .collect(toCollection(ArrayDeque::new));
-      // TODO: Can we reduce the memory impact of copying the entire input graph here?
-      this.edgesRemaining = com.google.common.graph.Graphs.copyOf(graph);
+      this.nonRootToInDegree =
+          graph
+              .nodes()
+              .stream()
+              .filter(node -> graph.inDegree(node) > 0)
+              .collect(toMap(node -> node, graph::inDegree, (a, b) -> b, HashMap::new));
     }
 
     @Override
@@ -76,14 +89,16 @@ public class Graphs {
       if (!roots.isEmpty()) {
         N next = roots.remove();
         for (N successor : graph.successors(next)) {
-          edgesRemaining.removeEdge(next, successor);
-          if (edgesRemaining.inDegree(successor) == 0) {
+          int newInDegree = nonRootToInDegree.get(successor) - 1;
+          nonRootToInDegree.put(successor, newInDegree);
+          if (newInDegree == 0) {
+            nonRootToInDegree.remove(successor);
             roots.add(successor);
           }
         }
         return next;
       } else {
-        checkState(edgesRemaining.edges().isEmpty(), "graph has at least one cycle");
+        checkState(nonRootToInDegree.isEmpty(), "graph has at least one cycle");
         return endOfData();
       }
     }
