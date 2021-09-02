052e3f7eb347f657c2a248b69a3ff6d2dcfd037a
Liu <shiyul3@fa21-cs527-017.cs.illinois.edu>
2021-09-02 10:54:28 -0500

    Fixed flaky test in mst

--- jung-algorithms/src/test/java/edu/uci/ics/jung/algorithms/shortestpath/TestMinimumSpanningTree.java
+++ jung-algorithms/src/test/java/edu/uci/ics/jung/algorithms/shortestpath/TestMinimumSpanningTree.java
@@ -30,6 +30,23 @@ public class TestMinimumSpanningTree extends TestCase {
 
     assertEquals(graph.nodes(), mst.nodes());
     assertEquals(graph.edges(), mst.edges());
+
+    // Add more tests.
+    MutableNetwork<String, Integer> graph2 = NetworkBuilder.directed().build();
+    graph2.addNode("B0");
+    graph2.addNode("B2");
+    graph2.addNode("B3");
+    graph2.addEdge("A", "B1", 0);
+    graph2.addEdge("A", "B2", 3);
+    graph2.addEdge("A", "B3", 5);
+
+    Network<String, Integer> mst2 = MinimumSpanningTree.extractFrom(graph2, e -> 1.0);
+
+    // Test mutableNetwork.
+    MutableNetwork<String, Integer> graph3 = NetworkBuilder.directed().build();
+
+    assertEquals(graph2.nodes(), mst2.nodes());
+    assertEquals(graph2.edges(), mst2.edges());
   }
 
   // TODO: add tests:
