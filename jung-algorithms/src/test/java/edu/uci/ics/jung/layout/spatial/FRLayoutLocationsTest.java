36889441b28a0d5d09bdf9aa429d51a9ce90b0bd
melanxoluk <melanxoluk@yandex.ru>
2025-04-25 17:13:29 +0500

    Apply google java format

--- jung-algorithms/src/test/java/edu/uci/ics/jung/layout/spatial/FRLayoutLocationsTest.java
+++ jung-algorithms/src/test/java/edu/uci/ics/jung/layout/spatial/FRLayoutLocationsTest.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2025, the JUNG Project and the Regents of the University
+ * of California.  All rights reserved.
+ *
+ * This software is open-source under the BSD license; see
+ * https://github.com/jrtom/jung/blob/master/LICENSE for a description.
+ */
 package edu.uci.ics.jung.layout.spatial;
 
 import com.google.common.graph.GraphBuilder;
@@ -10,43 +17,41 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Tests the fix for incorrect Y-coordinate distribution in FRLayoutAlgorithm.  
- * Previously, nodes were arranged in a square (width × width), ignoring the configured layout height.  
+ * Tests the fix for incorrect Y-coordinate distribution in FRLayoutAlgorithm. Previously, nodes
+ * were arranged in a square (width × width), ignoring the configured layout height.
  *
- * To verify the fix:  
- * 1. Generate enough nodes to exceed the layout bounds.  
- * 2. Ensure all nodes are positioned within the specified width * height area after layout.  
+ * <p>To verify the fix: 1. Generate enough nodes to exceed the layout bounds. 2. Ensure all nodes
+ * are positioned within the specified width * height area after layout.
+ *
+ * @author melanxoluk
  */
 public class FRLayoutLocationsTest {
-    private static final int layoutWidth = 2000;
-    private static final int layoutHeight = 500;
-    private static final int iterations = 200;
-    private static final int nodes = 20;
-    
-    private MutableGraph<Integer> graph;
-    private LayoutModel<Integer> layoutModel;
-    
-    @Test
-    public void testTargetCoordinates() {
-        graph = GraphBuilder.directed().build();
-        LoadingCacheLayoutModel.Builder<Integer, ?> builder =
-            LoadingCacheLayoutModel.<Integer>builder()
-                .setGraph(graph)
-                .setSize(layoutWidth, layoutHeight);
-        
-        layoutModel = new FRLayoutsTest.TestLayoutModel<>(builder, iterations);
+  private static final int layoutWidth = 2000;
+  private static final int layoutHeight = 500;
+  private static final int iterations = 200;
+  private static final int nodes = 20;
 
-        for (int i = 0; i < nodes; i++) {
-            graph.addNode(i);
-            layoutModel.set(i, Point.of(i, i));
-        }
+  @Test
+  public void testTargetCoordinates() {
+    MutableGraph<Integer> graph = GraphBuilder.directed().build();
+    LoadingCacheLayoutModel.Builder<Integer, ?> builder =
+        LoadingCacheLayoutModel.<Integer>builder()
+            .setGraph(graph)
+            .setSize(layoutWidth, layoutHeight);
 
-        FRLayoutAlgorithm algorithm = new FRLayoutAlgorithm();
-        layoutModel.accept(algorithm);
-        
-        Assert.assertTrue(
-            "All points should has coordinates into target layout size", 
-            layoutModel.getLocations().values().stream().allMatch(it -> it.y <= layoutHeight && it.x <= layoutWidth)
-        );
+    LayoutModel<Integer> layoutModel = new FRLayoutsTest.TestLayoutModel<>(builder, iterations);
+
+    for (int i = 0; i < nodes; i++) {
+      graph.addNode(i);
+      layoutModel.set(i, Point.of(i, i));
     }
+
+    FRLayoutAlgorithm algorithm = new FRLayoutAlgorithm();
+    layoutModel.accept(algorithm);
+
+    Assert.assertTrue(
+        "All points should has coordinates into target layout size",
+        layoutModel.getLocations().values().stream()
+            .allMatch(it -> it.y <= layoutHeight && it.x <= layoutWidth));
+  }
 }
